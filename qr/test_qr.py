2c18ad2d3171ad96935777baf3dfea20297ab283
Caspar Nettelbladt <caspar.n@gmail.com>
2024-04-12 00:49:32 +0200

    Format test file

--- qr/test_qr.py
+++ qr/test_qr.py
@@ -2,4 +2,7 @@ from .qr import decode_image
 
 
 def test_decode_image():
-    assert decode_image("./data/image.png") == "WIFI:S:Alpaca Guest;T:WPA;P:tyhjuz-dypjy3-Ciskow;;"
+    assert (
+        decode_image("./data/image.png")
+        == "WIFI:S:Alpaca Guest;T:WPA;P:tyhjuz-dypjy3-Ciskow;;"
+    )
